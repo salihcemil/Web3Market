9aa9768e9375d92fbff77187db7f806a4e02972a
Salih Cemil Cetin <salihcemil@gmail.com>
2022-07-31 17:24:04 +0400

    updatePayeeAddressOfCurrency and buyItem functions added, IERC20 interface imported

--- contracts/Web3Pay.sol
+++ contracts/Web3Pay.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Web3Pay is Ownable {
     string public constant name = "Web3Pay Contract";
@@ -9,7 +10,7 @@ contract Web3Pay is Ownable {
 
     mapping(uint256 => Item) items;
     //currencyAddress => paymentAddress
-    mapping(address => Currency) currencies;
+    mapping(IERC20 => Currency) currencies;
 
     struct Currency {
         string name;
@@ -19,18 +20,23 @@ contract Web3Pay is Ownable {
     struct Item {
         uint256 itemId;
         string itemName;
-        address currency;
+        IERC20 currency;
+        uint256 price;
         bool available;
     }
 
     event ItemAdded(
         uint256 indexed itemNo,
         string indexed name,
-        address currency
+        address indexed currency
     );
     event ItemDeactivated(uint256 indexed itemNo);
     event CurrencyAdded(string currencyName);
     event CurrencyDisabled(string currencyName);
+    event PayeeAddressUpdated(
+        address indexed currencyAddres,
+        address newPayeeAddress
+    );
 
     constructor(
         address _currencyERC20Address,
@@ -38,16 +44,23 @@ contract Web3Pay is Ownable {
         string memory _currencyName
     ) {
         Currency memory currency = Currency(_currencyName, _payeeAddress, true);
-        currencies[_currencyERC20Address] = currency;
+        currencies[IERC20(_currencyERC20Address)] = currency;
         itemCounter = 0;
     }
 
-    function addItem(string memory _itemName, address _currency)
-        external
-        onlyOwner
-    {
-        require(currencies[_currency].isActive, "Currency not found");
-        Item memory item = Item(itemCounter++, _itemName, _currency, true);
+    function addItem(
+        string memory _itemName,
+        address _currency,
+        uint256 _price
+    ) external onlyOwner {
+        require(currencies[IERC20(_currency)].isActive, "Currency not found");
+        Item memory item = Item(
+            itemCounter++,
+            _itemName,
+            IERC20(_currency),
+            _price,
+            true
+        );
         items[itemCounter++] = item;
         itemCounter++;
         emit ItemAdded(itemCounter--, _itemName, _currency);
@@ -68,21 +81,46 @@ contract Web3Pay is Ownable {
         string memory _currencyName
     ) external onlyOwner {
         require(
-            currencies[_ERC20ContractAddress].isActive,
+            currencies[IERC20(_ERC20ContractAddress)].isActive,
             "Currency already added"
         );
         Currency memory currency = Currency(_currencyName, _payeeAddress, true);
-        currencies[_ERC20ContractAddress] = currency;
+        currencies[IERC20(_ERC20ContractAddress)] = currency;
         emit CurrencyAdded(_currencyName);
     }
 
     function disableCurrency(address _ERC20ContractAddress) external onlyOwner {
         require(
-            currencies[_ERC20ContractAddress].isActive,
+            currencies[IERC20(_ERC20ContractAddress)].isActive,
             "Currency not active"
         );
-        currencies[_ERC20ContractAddress].isActive = false;
-        emit CurrencyDisabled(currencies[_ERC20ContractAddress].name);
+        currencies[IERC20(_ERC20ContractAddress)].isActive = false;
+        emit CurrencyDisabled(currencies[IERC20(_ERC20ContractAddress)].name);
+    }
+
+    function updatePayeeAddressOfCurrency(
+        address _currencyAddress,
+        address _newPayeeAddress
+    ) external onlyOwner {
+        require(
+            currencies[IERC20(_currencyAddress)].isActive,
+            "Currency is not available"
+        );
+        currencies[IERC20(_currencyAddress)].payeeAddress = _newPayeeAddress;
+        emit PayeeAddressUpdated(_currencyAddress, _newPayeeAddress);
+    }
+
+    function buyItem(uint256 _itemId) public {
+        require(items[_itemId].available, "Item is not available");
+        IERC20 token = items[_itemId].currency;
+        uint256 balance = token.balanceOf(msg.sender);
+        require(items[_itemId].price <= balance, "Buyer has no enough balance");
+
+        token.transferFrom(
+            msg.sender,
+            currencies[items[_itemId].currency].payeeAddress,
+            items[_itemId].price
+        );
     }
 
     function getOwner() public view returns (address) {
